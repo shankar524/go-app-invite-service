d3ea9f5c71324c79b0bf522b75d30a60d26ca62f
Shankar Acharya <shankar.acharya524@gmail.com>
2022-04-30 22:46:00 +0545

    add service

---
+++ api/service/token.go
@@ -0,0 +1,41 @@
+package service
+
+import (
+	"github.com/shankar524/go-app-invite-service/api/repository"
+	"github.com/shankar524/go-app-invite-service/models"
+	"github.com/shankar524/password-generator/src/text"
+)
+
+type TokenService struct {
+	tokenRepository repository.TokenRepository
+	textgenerator   text.Generator
+}
+
+func NewTokenService(repo repository.TokenRepository, textgenerator text.Generator) TokenService {
+	return TokenService{
+		tokenRepository: repo,
+		textgenerator:   textgenerator,
+	}
+}
+
+func (ts *TokenService) Create() (models.Token, error) {
+	token := models.Token{Value: ts.textgenerator.Generate(), Disabled: false}
+
+	return ts.tokenRepository.Save(token)
+}
+
+func (ts *TokenService) GetAll() ([]models.Token, error) {
+	return ts.tokenRepository.GetAll()
+}
+
+func (ts *TokenService) GetByID(id string) (models.Token, error) {
+	return ts.tokenRepository.GetByID(id)
+}
+
+func (ts *TokenService) DisableTokenByID(id string) (models.Token, error) {
+	return ts.tokenRepository.DisableTokenByID(id)
+}
+
+func (ts *TokenService) ValidateToken(token string) (bool, error) {
+	return ts.tokenRepository.ValidToken(token)
+}
